8712e173e3b7394a4505b16a86c7ecf3300de06b
HaleOIC <shinehale730@gmail.com>
2023-07-22 17:24:08 +0800

    finish ass2(version 0.1)

---
+++ assign/assignment2/codes/test.sh
@@ -0,0 +1,5 @@
+make
+# ./bwtsearch ./sam/sam.rlb ./sam/sam.idx "pp"
+# ./bwtsearch ./dummy/dummy.rlb ./dummy/dummy.idx "com"
+# ./bwtsearch ./sam2/sam2.rlb ./sam2/sam2.idx "bin"
+./bwtsearch ./sam3/sam3.rlb ./sam3/sam3.idx "an"
