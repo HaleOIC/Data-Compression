5b56986f3290567756610847cbbea2260bc9d927
HaleOIC <shinehale730@gmail.com>
2023-07-22 20:47:15 +0800

    update some files

--- assign/assignment2/codes/test.sh
+++ assign/assignment2/codes/test.sh
@@ -1,5 +1,6 @@
 make
 # ./bwtsearch ./sam/sam.rlb ./sam/sam.idx "pp"
-# ./bwtsearch ./dummy/dummy.rlb ./dummy/dummy.idx "com"
-# ./bwtsearch ./sam2/sam2.rlb ./sam2/sam2.idx "bin"
+# ./bwtsearch ./dummy/dummy.rlb ./dummy/dummy.idx "in"
+# ./bwtsearch ./dummy/dummy.rlb ./dummy/dummy.idx "in "
+# ./bwtsearch ./sam2/sam2.rlb ./sam2/sam2.idx "com"
 ./bwtsearch ./sam3/sam3.rlb ./sam3/sam3.idx "an"
